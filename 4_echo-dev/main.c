5611ec779af12267aa716218f33c0dced4103756
Douglas <su_zibin@163.com>
2015-06-04 17:37:11 +0800

    Tmp save

--- 4_echo-dev/main.c
+++ 4_echo-dev/main.c
@@ -23,6 +23,25 @@ struct file_operations echo_ops = {
 
 module_param(echo_buf_size, int, S_IRUGO);
 
+static inline void init_it(struct echo_dev *dev, int index)
+{
+	int err = 0;
+	dev_t devno = MKDEV(echo_major, echo_minor + index);
+
+	atomic_set(&dev->opt_counter, 0);
+	dev->data_len = 0;
+	dev->data_max_len = echo_buf_size;
+	dev->data = (char *)kmalloc(echo_buf_size * sizeof(char), GFP_KERNEL);
+	memset(dev->data, 0, echo_buf_size);
+	
+	mutex_init(&dev->mutex);
+	cdev_init(&dev->cdev, &echo_ops);
+	dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &echo_ops;
+	if ((err = cdev_add(&dev->cdev, devno, 1)))
+		printk(KERN_NOTICE "Error %d adding echo%d", err, index);
+}
+
 static int __init module_start(void)
 {
 	int i, err = 0;
@@ -45,21 +64,7 @@ static int __init module_start(void)
 	echo_devs = (struct echo_dev *)
 		    kmalloc(ECHO_NR_DEVS * sizeof(*echo_devs), GFP_KERNEL);
 	for (i = 0; i < ECHO_NR_DEVS; ++i) {
-		devno = MKDEV(echo_major, echo_minor + i);
-		atomic_set(&(echo_devs + i)->opt_counter, 0);
-		(echo_devs + i)->data_len = 0;
-		(echo_devs + i)->data_max_len = echo_buf_size;
-		(echo_devs + i)->data = (char *)
-		    kmalloc(echo_buf_size * sizeof(char), GFP_KERNEL);
-		memset((echo_devs + i)->data, 0, echo_buf_size);
-		mutex_init(&(echo_devs + i)->mutex);
-		cdev_init(&(echo_devs + i)->cdev, &echo_ops);
-		(echo_devs + i)->cdev.owner = THIS_MODULE;
-		(echo_devs + i)->cdev.ops = &echo_ops;
-		err = cdev_add(&(echo_devs + i)->cdev, devno, 1);
-		if (err) {
-			printk(KERN_NOTICE "Error %d adding echo%d", err, i);
-		}
+		init_it(echo_devs + i, i);
 	}
 
 	return 0;
@@ -74,8 +79,10 @@ static void __exit module_end(void)
 	if (echo_devs) {
 		for (i = 0; i < ECHO_NR_DEVS; ++i) {
 			cdev_del(&(echo_devs + i)->cdev);
+			kfree((echo_devs + i)->data);
 		}
 	}
+	kfree(echo_devs);
 	unregister_chrdev_region(devno, ECHO_NR_DEVS);
 }
 
@@ -83,5 +90,5 @@ module_init(module_start);
 module_exit(module_end);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Doug");
+MODULE_AUTHOR("Douglas");
 
