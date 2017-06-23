8099e9a1f1e353cb1250f01da536f2f99f0b2e38
d0u9 <d0u9.su@outlook.com>
2017-06-23 08:29:23 +0800

    06_completion

---
+++ 06_completion/load_module.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+module="completion"
+device="completion"
+mode="666"
+group=0
+
+function load() {
+    insmod ./$module.ko $* || exit 1
+
+    rm -f /dev/${device}
+
+    major=$(awk -v device="$device" '$2==device {print $1}' /proc/devices)
+    mknod /dev/${device} c $major 0
+
+    chgrp $group /dev/$device
+    chmod $mode /dev/$device
+}
+
+function unload() {
+    rm -f /dev/${device}
+    rmmod $module || exit 1
+}
+
+arg=${1:-"load"}
+case $arg in
+    load)
+        load ;;
+    unload)
+        unload ;;
+    reload)
+        ( unload )
+        load
+        ;;
+    *)
+        echo "Usage: $0 {load | unload | reload}"
+        echo "Default is load"
+        exit 1
+        ;;
+esac
---
+++ 06_completion/main.c
@@ -0,0 +1,110 @@
+#include <linux/module.h>
+#include <linux/init.h> 
+#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/types.h>
+#include <linux/completion.h>
+
+#include "main.h"
+
+static int completion_major = 0, completion_minor = 0;
+
+static struct completion_dev completion_dev;
+
+static
+int completion_open(struct inode *inode, struct file *filp)
+{
+	PDEBUG("%s() is invoked\n", __FUNCTION__);
+
+	filp->private_data = container_of(inode->i_cdev,
+					  struct completion_dev, cdev);
+
+	return 0;
+}
+
+static
+ssize_t completion_read(struct file *filp, char __user *buf, size_t count, loff_t *pos)
+{
+	struct completion_dev *dev = filp->private_data;
+
+	PDEBUG("%s() is invoked\n", __FUNCTION__);
+
+	PDEBUG("process %d(%s) is going to sleep\n", current->pid, current->comm);
+	wait_for_completion(&dev->completion);
+	PDEBUG("awoken %d(%s)\n", current->pid, current->comm);
+
+	return 0;
+}
+
+static
+ssize_t completion_write(struct file *filp, const char __user *buf, size_t count,
+		       loff_t *pos)
+{
+	struct completion_dev *dev = filp->private_data;
+
+	PDEBUG("%s() is invoked\n", __FUNCTION__);
+
+	PDEBUG("process %d(%s) awakening the readers...\n",
+	       current->pid, current->comm);
+	complete(&dev->completion);
+
+	return count;
+}
+
+static struct file_operations completion_fops = {
+	.owner = THIS_MODULE,
+	.open  = completion_open, 
+	.read  = completion_read,
+	.write = completion_write,
+};
+
+static
+int __init m_init(void)
+{
+	int err = 0;
+	dev_t devno;
+
+	printk(KERN_WARNING MODULE_NAME " is loaded\n");
+
+	init_completion(&completion_dev.completion);
+
+	err = alloc_chrdev_region(&devno, completion_minor, 1, MODULE_NAME);
+	if (err < 0) {
+		PDEBUG("Cant't get major");
+		return err;
+	}
+	completion_major = MAJOR(devno);
+
+	cdev_init(&completion_dev.cdev, &completion_fops);
+
+	devno = MKDEV(completion_major, completion_minor);
+	err = cdev_add(&completion_dev.cdev, devno, 1);
+	if (err) {
+		PDEBUG("Error(%d): Adding completion device error\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
+static
+void __exit m_exit(void)
+{
+	dev_t devno;
+
+	printk(KERN_WARNING MODULE_NAME " unloaded\n");
+
+	cdev_del(&completion_dev.cdev);
+
+	devno = MKDEV(completion_major, completion_minor);
+	unregister_chrdev_region(devno, 1);
+}
+
+module_init(m_init);
+module_exit(m_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("d0u9");
+MODULE_DESCRIPTION("Example of Kernel's completion mechanism");
