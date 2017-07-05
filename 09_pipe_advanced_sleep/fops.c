71134e344947a4adee50ed7128981b3255e77500
d0u9 <d0u9.su@outlook.com>
2017-07-05 08:58:30 +0800

    09_pipe_advanced_sleep

---
+++ 09_pipe_advanced_sleep/fops.c
@@ -0,0 +1,149 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+
+#include "main.h"
+#include "fops.h"
+
+int pipe_open(struct inode *inode, struct file *filp)
+{
+	PDEBUG("%s() is invoked\n", __FUNCTION__);
+
+	filp->private_data = container_of(inode->i_cdev, struct pipe_dev, cdev);
+
+	return 0;
+}
+
+ssize_t pipe_read(struct file *filp, char __user *buff, size_t count,
+	loff_t *f_pos)
+{
+	int retval = 0;
+	struct pipe_dev *dev = filp->private_data;
+
+	PDEBUG("%s() is invoked\n", __FUNCTION__);
+
+	if (mutex_lock_interruptible(&dev->mutex))
+		return -ERESTARTSYS;
+
+	while (!dev->buff_len) {
+		DEFINE_WAIT(wait);
+
+		mutex_unlock(&dev->mutex);
+
+		if (filp->f_flags & O_NONBLOCK)
+			return -EAGAIN;
+
+		PDEBUG("read: process %d(%s) is going to sleep\n",
+		       current->pid, current->comm);
+
+		prepare_to_wait(&dev->rd_queue, &wait, TASK_INTERRUPTIBLE);
+		if (!dev->buff_len)
+			schedule();
+		finish_wait(&dev->rd_queue, &wait);
+
+		if (signal_pending(current))
+			return -ERESTARTSYS;
+
+		if (mutex_lock_interruptible(&dev->mutex))
+			return -ERESTARTSYS;
+	}
+
+	if (count > dev->buff_len - *f_pos)
+		count = dev->buff_len - *f_pos;
+
+	if (copy_to_user(buff, dev->buff + *f_pos, count)) {
+		PDEBUG("copy to user error!\n");
+		retval = -EFAULT;
+		goto copy_error;
+	}
+
+	PDEBUG("read: f_pos=%lld, count=%lu, buff_len=%d\n",
+	       *f_pos, count, dev->buff_len);
+
+	*f_pos += count;
+
+	// all data in the buff have been read
+	if (*f_pos >= dev->buff_len) {
+		dev->buff_len = 0;
+		*f_pos = 0;
+		PDEBUG("read: process %d(%s) awakening the writers...\n",
+		       current->pid, current->comm);
+		wake_up_interruptible(&dev->wr_queue);
+	}
+
+	retval = count;
+
+copy_error:
+	mutex_unlock(&dev->mutex);
+	return retval;
+}
+
+ssize_t pipe_write(struct file *filp, const char __user *buff,
+		   size_t count, loff_t *f_pos)
+{
+	int retval = 0;
+	struct pipe_dev *dev = filp->private_data;
+
+	PDEBUG("%s() is invoked\n", __FUNCTION__);
+
+	if (mutex_lock_interruptible(&dev->mutex))
+		return -ERESTARTSYS;
+
+	while (dev->buff_len) {
+		DEFINE_WAIT(wait);
+
+		mutex_unlock(&dev->mutex);
+		if (filp->f_flags & O_NONBLOCK)
+			return -EAGAIN;
+
+		PDEBUG("process %d(%s) is going to sleep\n",
+		       current->pid, current->comm);
+
+		prepare_to_wait(&dev->wr_queue, &wait, TASK_INTERRUPTIBLE);
+		if (dev->buff_len)
+			schedule();
+		finish_wait(&dev->wr_queue, &wait);
+
+		if (signal_pending(current))
+			return -ERESTARTSYS;
+
+		if (mutex_lock_interruptible(&dev->mutex))
+			return -ERESTARTSYS;
+	}
+
+	if (count > BUFF_SIZE - *f_pos)
+		count = BUFF_SIZE - *f_pos;
+
+	if (copy_from_user(dev->buff + *f_pos, buff, count)) {
+		PDEBUG("write: copy to user error!\n");
+		retval = -EFAULT;
+		goto copy_error;
+	}
+
+	PDEBUG("write: f_pos=%lld, count=%lu, buff_len=%d\n",
+	       *f_pos, count, dev->buff_len);
+
+	// we have successfully write something in the buff
+	if (count > 0) {
+		dev->buff_len = count;
+		*f_pos = 0;
+
+		PDEBUG("write: process %d(%s) awakening the readers...\n",
+		       current->pid, current->comm);
+		wake_up_interruptible(&dev->rd_queue);
+	}
+
+	retval = count;
+
+copy_error:
+	mutex_unlock(&dev->mutex);
+	return retval;
+}
+
+
---
+++ 09_pipe_advanced_sleep/load_module.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+module="pipe_advanced_sleep"
+device="pipe_advanced_sleep"
+mode="666"
+group=0
+
+function load() {
+    insmod ./$module.ko $* || exit 1
+
+    rm -f /dev/${device}[0-3]
+
+    major=$(awk -v device="$device" '$2==device {print $1}' /proc/devices)
+    mknod /dev/${device}0 c $major 0
+    mknod /dev/${device}1 c $major 1
+    mknod /dev/${device}2 c $major 2
+    mknod /dev/${device}3 c $major 3
+
+    chgrp $group /dev/$device[0-3]
+    chmod $mode /dev/$device[0-3]
+}
+
+function unload() {
+    rm -f /dev/${device}[0-3]
+    rmmod $module || exit 1
+}
+
+arg=${1:-"load"}
+case $arg in
+    load)
+        load ;;
+    unload)
+        unload ;;
+    reload)
+        ( unload )
+        load
+        ;;
+    *)
+        echo "Usage: $0 {load | unload | reload}"
+        echo "Default is load"
+        exit 1
+        ;;
+esac
+
