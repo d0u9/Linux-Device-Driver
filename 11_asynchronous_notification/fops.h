c993834c291141c93eedd65a97077bc484bb22ae
Rouzip <1226015390@qq.com>
2018-08-17 16:30:04 +0800

    rename the function

--- 11_asynchronous_notification/fops.h
+++ 11_asynchronous_notification/fops.h
@@ -5,7 +5,7 @@ extern int async_notify_open(struct inode *inode, struct file *filp);
 extern ssize_t async_notify_read(struct file *filp, char __user *buff,
 				 size_t count, loff_t *f_pos);
 extern int async_notify_fasync(int fd, struct file *filp, int mode);
-extern int async_release(struct inode *inode, struct file *filp);
+extern int async_notify_release(struct inode *inode, struct file *filp);
 
 extern ssize_t async_notify_write(struct file *filp, const char __user *buff,
 				  size_t count, loff_t *f_pos);
