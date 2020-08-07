8f4d1a4831134121da452c6fbfab5b9e20112d1f
d0u9 <d0u9.su@outlook.com>
2020-08-07 15:11:57 +0800

    Add new 15_short_ioport

---
+++ _15/main.h
@@ -0,0 +1,30 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#define PRINTK_LEVEL KERN_DEBUG
+
+#ifndef MUDULE_NAME
+#define MODULE_NAME		"short"
+#endif
+
+#undef PDEBUG
+#ifdef ENABLE_DEBUG
+#  ifdef __KERNEL__
+#    define PDEBUG(format, args...) printk(PRINTK_LEVEL MODULE_NAME ": " format, ##args)
+#  else
+#    define PDEBUG(format, args...) fprintf(stderr, MODULE_NAME ": " format, ##args)
+#  endif
+#else
+#  define PDEBUG(format, args...)
+#endif
+
+#define SHORT_NR_PORTS	8		/* use 8 ports by default */
+
+struct short_dev {
+	int block_counter;		//record how many blocks now in the list
+	struct mutex mutex;
+	struct cdev cdev;
+	struct list_head block_list;	//list of storage blocks
+};
+
+#endif
